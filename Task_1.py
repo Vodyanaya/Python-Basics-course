fb1a3b8c336c4aaec21059acd1b2cc7bc3bad765
Vodyanaya <vodyanayavorona@gmail.com>
2021-07-30 15:19:54 +0200

    Python basics
    Practice (Homework)

---
+++ Task_1.py
@@ -0,0 +1,17 @@
+
+# Задание 1
+
+# Поработайте с переменными, создайте несколько, выведите на экран,
+# запросите у пользователя несколько чисел и строк и сохраните в переменные, выведите на экран
+
+random_var = [1, 2, 3]
+random_var_0 = "I'm not quite sure, what I am supposed to do here"
+random_var_1 = [n + 1 for n in random_var]
+
+print(random_var_1)
+print(random_var, "\n" + random_var_0)
+
+number_1 = int(input("Random number more than 1: "))
+random_string = str(input("Random animal: "))
+
+print(number_1, random_string + "s")
